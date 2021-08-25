eb134e13b5195a0a471c8aeaf1feb63f6f7eeda5
destiny0114 <42262712+destiny0114@users.noreply.github.com>
2021-08-25 14:25:44 +0800

    Revert "Merge pull request #1 from destiny0114/development"
    This reverts commit 79a75783e313e6da461a26ffcd6a78bf9d2f8df6, reversing
    changes made to 1003611b77abfede681d876e0ff3edf071fa1826.

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
-/*
- * Project: nutritionx
- * Created Date: Wednesday August 25th 2021
- * Author: Keena Levine (ahhao0114@gmail.com)
- * -----
- * Last Modified: Wednesday, 25th August 2021 1:43:02 pm
- * Modified By: Keena Levine (ahhao0114@gmail.com)
- * -----
- * MIT License
- * Copyright (c) 2021 Keena Levine
- */
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const App: React.FC = () => {
-	return <div>Hello World</div>;
-};
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
